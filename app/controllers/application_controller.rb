9e622b657d8bbb073da047eb12ae4bcfef3525cc
Michael Lobel <mglobel@gmail.com>
2015-09-04 02:27:57 -0500

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :find_user
+
+  private
+    def find_user
+      @user = Account.find_by_personemail 'trainerbob@gmail.com' # Bob.
+    end
+end
