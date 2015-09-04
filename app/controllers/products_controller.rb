2334a29ae159262fb0ce33cbf42809777a885b2b
Michael Lobel <mglobel@gmail.com>
2015-09-04 16:44:09 -0500

    update products_controller.rb (deja vu)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,7 @@ class ProductsController < ApplicationController
     )
 
     @pricebook_entry = PricebookEntry.create(
-      serviceprovider__c:  user.sfid,
+      serviceprovider__c:  @user.sfid,
       name:                "#{@user.name}'s Hourly Rate",
       product2id:          product.sfid,
       pricebook2id:        pricebook.sfid,
